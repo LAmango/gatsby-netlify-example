df2f32dcaabbef1108a167abba13112126f9c134
Lucas Albano <lucas@albano.us>
2019-07-25 19:46:10 -0400

    added to index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 class Index extends React.Component {
   render() {
     console.log("test")
-    return <div>Hello world!</div>
+    return <div>Hello world! nice to meet you!</div>
   }
 }
 export default Index
